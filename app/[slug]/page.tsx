2647c89b961847b28038a7f886349eda7af2d187
Asli Akbuga <aslibusra.20@gmail.com>
2026-07-22 15:17:38 +0300

    Drive entegrasyonu ve dosya güncellemeleri yapıldı

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -67,7 +67,7 @@ export default function WeddingUploadPage() {
 
     try {
       // Apps Script Web App URL'ni buraya yapıştıracaksın kanka
-      const scriptUrl = 'BURAYA_GOOGLE_APPS_SCRIPT_WEB_APP_URLINI_YAZ';
+      const scriptUrl = 'https://script.google.com/macros/s/AKfycbzYii13aWVEcGgzMyrnarSZ-LEX3c-tAuRxL-pReiTuCHnUKYZJ1mxqZW1RRfEjZihE/exec';
 
       for (let i = 0; i < selectedFiles.length; i++) {
         const file = selectedFiles[i];
