2cafd78d6ac311a5e0b84261aa3f3d28a5defddb
Asli Akbuga <aslibusra.20@gmail.com>
2026-07-22 15:22:05 +0300

    Google Apps Script URL'si baglandi, fotograflar Drive'a akiyor

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -67,7 +67,7 @@ export default function WeddingUploadPage() {
 
     try {
       // Apps Script Web App URL'ni buraya yapıştıracaksın kanka
-      const scriptUrl = 'https://script.google.com/macros/s/AKfycbzYii13aWVEcGgzMyrnarSZ-LEX3c-tAuRxL-pReiTuCHnUKYZJ1mxqZW1RRfEjZihE/exec';
+      const scriptUrl = 'https://script.google.com/macros/s/AKfycbyq7uC01OVUbSWVk95ZXsI9QuYgn7E2_TEhUXhY9T0/dev';
 
       for (let i = 0; i < selectedFiles.length; i++) {
         const file = selectedFiles[i];
